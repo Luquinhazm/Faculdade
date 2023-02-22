3cf0b81f89ac9e48b30a946d2d3fee1d5e6814be
Lucas Mendes <lucsmends09@gmail.com>
2023-02-22 19:27:33 -0300

    Adicionar arquivos de projeto.

---
+++ Aula03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ProgramIdade
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Olá Digite sua idade em Anos = ");
+        int anos = int.Parse(Console.ReadLine());
+        Console.Write("Agora digite sua idade em Meses = ");
+        int meses = int.Parse(Console.ReadLine());
+        Console.Write("Por ultimo digite os Dias:");
+        int dias = int.Parse(Console.ReadLine());
+        int idadeDias = anos * 365 +meses * + dias;
+        Console.WriteLine("idade em dias = {0}", idadeDias);
+    }
+}
